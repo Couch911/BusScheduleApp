fcf3b8c7d4187bb6a68ebdd1616013152cd8eed9
Couch911 <goglelikegogle@gmail.com>
2026-04-25 15:28:25 +0300

    Add project files.

---
+++ BusScheduleApp/DataAccess.cs
@@ -0,0 +1,48 @@
+﻿using Npgsql; // Підключаємо драйвер PostgreSQL 
+using System.Collections.Generic;
+using System;
+
+namespace BusScheduleApp
+{
+    public class DataAccess
+    {
+        [cite_start]// Рядок з'єднання (пароль введіть свій) 
+        public string connStr = "Host=localhost; Username=postgres; Password=ВАШ_ПАРОЛЬ; Database=ROSKLADAVTOBUSIV";
+        public List<BusTrip> bList = new List<BusTrip>();
+
+        public DataAccess()
+        {
+            OpenDbFile();
+        }
+
+        private void OpenDbFile()
+        {
+            try
+            {
+                using (var conn = new NpgsqlConnection(connStr))
+                {
+                    conn.Open();
+                    string sql = "SELECT * FROM bus_schedule"; // Ваша таблиця 
+                    using (var cmd = new NpgsqlCommand(sql, conn))
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            bList.Add(new BusTrip(
+                                (int)reader["id"],
+                                reader["route_number"].ToString(),
+                                reader["destination"].ToString(),
+                                (TimeSpan)reader["departure_time"],
+                                (int)reader["free_seats"]
+                            ));
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Windows.MessageBox.Show("Помилка БД: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ BusScheduleApp/Globals.cs
@@ -0,0 +1,21 @@
+﻿namespace BusScheduleApp
+{
+    [cite_start]// Клас, який описує поля вашої таблиці в БД 
+    public class BusTrip
+    {
+        public int id { get; set; }
+        public string routeNumber { get; set; }
+        public string destination { get; set; }
+        public System.TimeSpan departureTime { get; set; }
+        public int freeSeats { get; set; }
+
+        public BusTrip(int idNum, string rN, string dest, System.TimeSpan dT, int fS)
+        {
+            this.id = idNum;
+            this.routeNumber = rN;
+            this.destination = dest;
+            this.departureTime = dT;
+            this.freeSeats = fS;
+        }
+    }
+}
